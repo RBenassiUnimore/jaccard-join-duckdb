3049a59a9c4db398b0e372515fff8f4e3b5d3322
riccardobenassi <riccardobenassi98@gmail.com>
2023-03-04 12:39:05 +0100

    refactoring

--- py_duckdb/similarity_join/join/jaccard_join.py
+++ py_duckdb/similarity_join/join/jaccard_join.py
@@ -16,19 +16,19 @@ def jaccard_join(
         r_join_attr: str,
         tokenizer: tokenizers.Tokenizer,
         threshold: float,
-        out_table_name: str,
+        out_table: str,
         l_out_prefix: str = 'l_',
         r_out_prefix: str = 'r_'
 ):
     if l_table:
         if l_table == r_table or not r_table:
             _JaccardSelfJoin(
-                con, l_table, l_key_attr, l_join_attr, tokenizer, threshold, out_table_name, l_out_prefix, r_out_prefix
+                con, l_table, l_key_attr, l_join_attr, tokenizer, threshold, out_table, l_out_prefix, r_out_prefix
             ).do_join()
         else:
             _JaccardJoin(
                 con, l_table, r_table, l_key_attr, r_key_attr, l_join_attr, r_join_attr, tokenizer, threshold,
-                out_table_name, l_out_prefix, r_out_prefix
+                out_table, l_out_prefix, r_out_prefix
             ).do_join()
     return con
 
@@ -43,58 +43,24 @@ def jaccard_join_brute_force(
         r_join_attr: str,
         tokenizer: tokenizers.Tokenizer,
         threshold: float,
-        out_table_name: str,
+        out_table: str,
         l_out_prefix: str = 'l_',
         r_out_prefix: str = 'r_'
 ):
     if l_table:
         if l_table == r_table or not r_table:
             _JaccardSelfJoin(
-                con, l_table, l_key_attr, l_join_attr, tokenizer, threshold, out_table_name, l_out_prefix, r_out_prefix
+                con, l_table, l_key_attr, l_join_attr, tokenizer, threshold, out_table, l_out_prefix, r_out_prefix
             ).do_brute_force_join()
         else:
             _JaccardJoin(
                 con, l_table, r_table, l_key_attr, r_key_attr, l_join_attr, r_join_attr, tokenizer, threshold,
-                out_table_name, l_out_prefix, r_out_prefix
+                out_table, l_out_prefix, r_out_prefix
             ).do_brute_force_join()
     return con
 
 
 class _JaccardTemplateJoin(ABC):
-
-    def __init__(
-            self,
-            con: duckdb.DuckDBPyConnection,
-            l_table: str,
-            r_table: str,
-            l_key_attr: str,
-            r_key_attr: str,
-            l_join_attr: str,
-            r_join_attr: str,
-            tokenizer: tokenizers.Tokenizer,
-            threshold: float,
-            out_table_name: str,
-            l_out_prefix: str,
-            r_out_prefix: str
-    ):
-        self._con = con
-        self._l_table = l_table
-        self._r_table = r_table
-        self._l_key_attr = l_key_attr
-        self._r_key_attr = r_key_attr
-        self._l_join_attr = l_join_attr
-        self._r_join_attr = r_join_attr
-        self._t = threshold
-        self._tokenizer = tokenizer
-        self._out_table_name = out_table_name
-        self._l_out_prefix = l_out_prefix
-        self._r_out_prefix = r_out_prefix
-
-        self._l_count = 0
-        self._r_count = 0
-        self._widow_placeholder = 0
-        self._r = ''
-        self._s = ''
 
     def do_join(self):
         self.build_input()
@@ -151,15 +117,17 @@ class _JaccardSelfJoin(_JaccardTemplateJoin):
             f"drop table if exists {INPUT_TABLE}"
         ).execute(
             f"create table {INPUT_TABLE} as "
-            f"select {self._l_key_attr} as id, {self._l_join_attr} as val "
-            f"from '{self._l_table}' "
+            f"select {self._key_attr} as id, {self._join_attr} as val "
+            f"from '{self._table}' "
         )
 
     def tokenize(self):
         self._con.execute(
             f"drop table if exists {TOKENS_VIEW}"
         ).execute(
-            f"create table {TOKENS_VIEW} as " + self._tokenizer.query()
+            f"create table {TOKENS_VIEW} as " + self._tokenizer.query(
+                from_table=INPUT_TABLE, key=self._key_attr, val=self._join_attr
+            )
         ).execute(
             f"drop table if exists {INPUT_TABLE}"
         )
@@ -211,12 +179,12 @@ class _JaccardSelfJoin(_JaccardTemplateJoin):
 
     def matches(self):
         self._con.execute(
-            f"drop table if exists {self._out_table_name}"
+            f"drop table if exists {self._out_table}"
         ).execute(
             # Start from the last match included to include the pairs in which the prefixes match entirely but the
             # suffixes do not match at all
-            f"create table {self._out_table_name} as "
-            f"select L.id as {self._l_out_prefix}{self._l_key_attr}, R.id as {self._r_out_prefix}{self._r_key_attr} "
+            f"create table {self._out_table} as "
+            f"select L.id as {self._l_out_prefix}{self._key_attr}, R.id as {self._r_out_prefix}{self._key_attr} "
             f"from {TOKENS_DOC_FREQ_VIEW} L, {TOKENS_DOC_FREQ_VIEW} R, {CANDIDATE_SET_VIEW} c "
             "where c.Lid = L.id "
             "and c.Rid = R.id "
@@ -233,10 +201,10 @@ class _JaccardSelfJoin(_JaccardTemplateJoin):
 
     def matches_brute_force(self):
         self._con.execute(
-            f"drop table if exists {self._out_table_name}"
+            f"drop table if exists {self._out_table}"
         ).execute(
-            f"create table {self._out_table_name} as "
-            f"select L.id as {self._l_out_prefix}{self._l_key_attr}, R.id as {self._r_out_prefix}{self._r_key_attr} "
+            f"create table {self._out_table} as "
+            f"select L.id as {self._l_out_prefix}{self._key_attr}, R.id as {self._r_out_prefix}{self._key_attr} "
             f"from {TOKENS_VIEW} as L, {TOKENS_VIEW} as R "
             "where L.token = R.token "
             "and L.id < R.id "
@@ -250,7 +218,6 @@ class _JaccardSelfJoin(_JaccardTemplateJoin):
             f"drop table if exists {TOKENS_VIEW};"
             f"drop table if exists {DOC_FREQ_VIEW};"
             f"drop table if exists {TOKENS_DOC_FREQ_VIEW};"
-            f"drop table if exists {PREFIXES_VIEW};"
             f"drop table if exists {CANDIDATE_SET_VIEW};"
         )
 
@@ -262,14 +229,20 @@ class _JaccardSelfJoin(_JaccardTemplateJoin):
             join_attr: str,
             tokenizer: tokenizers.Tokenizer,
             threshold: float,
-            out_table_name: str,
+            out_table: str,
             l_out_prefix: str,
             r_out_prefix: str
     ):
-        super().__init__(
-            con, table, '', key_attr, key_attr, join_attr, '', tokenizer, threshold, out_table_name,
-            l_out_prefix, r_out_prefix
-        )
+        self._con = con
+        self._tokenizer = tokenizer
+        self._t = threshold
+        self._out_table = out_table
+
+        self._table = table
+        self._key_attr = key_attr
+        self._join_attr = join_attr
+        self._l_out_prefix = l_out_prefix
+        self._r_out_prefix = r_out_prefix
 
 
 class _JaccardJoin(_JaccardTemplateJoin):
@@ -280,19 +253,19 @@ class _JaccardJoin(_JaccardTemplateJoin):
             f"drop table if exists r_{INPUT_TABLE};"
         ).execute(
             f"create table l_{INPUT_TABLE} as "
-            f"select {self._l_key_attr} as id, {self._l_join_attr} as val "
-            f"from '{self._l_table}' "
+            f"select {self._l['key_attr']} as id, {self._l['join_attr']} as val "
+            f"from '{self._l['table']}' "
         ).execute(
             f"create table r_{INPUT_TABLE} as "
-            f"select {self._r_key_attr} as id, {self._r_join_attr} as val "
-            f"from '{self._r_table}' "
+            f"select {self._r['key_attr']} as id, {self._r['join_attr']} as val "
+            f"from '{self._r['table']}' "
         )
 
-        self._l_count = self._con.execute(
+        self._l['count'] = self._con.execute(
             "select count(*) "
             f"from l_{INPUT_TABLE}"
         ).fetchall()[0][0]
-        self._r_count = self._con.execute(
+        self._r['count'] = self._con.execute(
             "select count(*) "
             f"from r_{INPUT_TABLE}"
         ).fetchall()[0][0]
@@ -317,7 +290,7 @@ class _JaccardJoin(_JaccardTemplateJoin):
     def document_frequency(self):
         # the document frequency of widow tokens is the max possible df + 1
         # this works both as unambiguous placeholder and to put widow tokens at the end in the ordering heuristic
-        self._widow_placeholder = self._l_count * self._r_count + 1
+        self._widow_placeholder = self._l['count'] * self._r['count'] + 1
 
         self._con.execute(
             f"drop table if exists full_outer_{DOC_FREQ_VIEW}"
@@ -347,9 +320,9 @@ class _JaccardJoin(_JaccardTemplateJoin):
         )
 
         self._con.execute(
-            f"drop table if exists l_{TOKENS_DOC_FREQ_VIEW}"
+            f"drop table if exists {self._l['out_prefix']}{TOKENS_DOC_FREQ_VIEW}"
         ).execute(
-            f"create table l_{TOKENS_DOC_FREQ_VIEW} as "
+            f"create table {self._l['out_prefix']}{TOKENS_DOC_FREQ_VIEW} as "
             f"select l_{TOKENS_VIEW}.*, {DOC_FREQ_VIEW}.df "
             f", row_number() OVER (PARTITION BY id ORDER BY df, l_{TOKENS_VIEW}.token) as pos "
             f"from l_{TOKENS_VIEW}, {DOC_FREQ_VIEW} "
@@ -358,9 +331,9 @@ class _JaccardJoin(_JaccardTemplateJoin):
             f"drop table if exists l_{TOKENS_VIEW}"
         )
         self._con.execute(
-            f"drop table if exists r_{TOKENS_DOC_FREQ_VIEW}"
+            f"drop table if exists {self._r['out_prefix']}{TOKENS_DOC_FREQ_VIEW}"
         ).execute(
-            f"create table r_{TOKENS_DOC_FREQ_VIEW} as "
+            f"create table {self._r['out_prefix']}{TOKENS_DOC_FREQ_VIEW} as "
             f"select r_{TOKENS_VIEW}.*, {DOC_FREQ_VIEW}.df "
             f", row_number() OVER (PARTITION BY id ORDER BY df, r_{TOKENS_VIEW}.token) as pos "
             f"from r_{TOKENS_VIEW}, {DOC_FREQ_VIEW} "
@@ -375,41 +348,41 @@ class _JaccardJoin(_JaccardTemplateJoin):
 
     def prefixes(self):
         self._con.execute(
-            f"drop table if exists l_{PREFIXES_VIEW}"
+            f"drop table if exists {self._l['out_prefix']}{PREFIXES_VIEW}"
         ).execute(
-            f"create table l_{PREFIXES_VIEW} as "
+            f"create table {self._l['out_prefix']}{PREFIXES_VIEW} as "
             "select id, len, token, pos, df "
-            f"FROM l_{TOKENS_DOC_FREQ_VIEW} "
+            f"FROM {self._l['out_prefix']}{TOKENS_DOC_FREQ_VIEW} "
             f"where len - pos + 1 >= (len * 2 * {self._t} / (1+{self._t})) "  # indexing prefix
         ).execute(
-            f"drop table if exists r_{PREFIXES_VIEW}"
+            f"drop table if exists {self._r['out_prefix']}{PREFIXES_VIEW}"
         ).execute(
-            f"create table r_{PREFIXES_VIEW} as "
+            f"create table {self._r['out_prefix']}{PREFIXES_VIEW} as "
             "select id, len, token, pos, df "
-            f"FROM r_{TOKENS_DOC_FREQ_VIEW} "
+            f"FROM {self._r['out_prefix']}{TOKENS_DOC_FREQ_VIEW} "
             f"where len - pos + 1 >= (len * 2 * {self._t} / (1+{self._t})) "  # indexing prefix
         )
 
         l_widows = self._con.execute(
             "select count(*) "
-            f"from l_{PREFIXES_VIEW} "
+            f"from {self._l['out_prefix']}{PREFIXES_VIEW} "
             f"where df = {self._widow_placeholder}"
         ).fetchall()[0][0]
 
         r_widows = self._con.execute(
             "select count(*) "
-            f"from r_{PREFIXES_VIEW} "
+            f"from {self._r['out_prefix']}{PREFIXES_VIEW} "
             f"where df = {self._widow_placeholder}"
         ).fetchall()[0][0]
 
-        self._r, self._s = ('l_', 'r_') if l_widows > r_widows else ('r_', 'l_')
+        self._R, self._S = (self._l, self._r) if l_widows > r_widows else (self._r, self._l)
 
         self._con.execute(
-            f"drop table if exists {self._s}{PREFIXES_VIEW}"
+            f"drop table if exists {self._S['out_prefix']}{PREFIXES_VIEW}"
         ).execute(
-            f"create table {self._s}{PREFIXES_VIEW} as "
+            f"create table {self._S['out_prefix']}{PREFIXES_VIEW} as "
             "select id, len, token, pos "
-            f"FROM {self._s}{TOKENS_DOC_FREQ_VIEW} "
+            f"FROM {self._S['out_prefix']}{TOKENS_DOC_FREQ_VIEW} "
             f"where len - pos + 1 >= (len * {self._t}) "  # probing prefix
         )
 
@@ -418,32 +391,30 @@ class _JaccardJoin(_JaccardTemplateJoin):
             f"drop table if exists {CANDIDATE_SET_VIEW}"
         ).execute(
             f"CREATE table {CANDIDATE_SET_VIEW} AS ("
-            "SELECT rpfx.id AS Rid, spfx.id AS Sid "
-            ", MAX(rpfx.pos) as RmaxPos, MAX(spfx.pos) as SmaxPos, count(*) as pfxOverlap "
-            f"FROM {self._r}{PREFIXES_VIEW} rpfx, {self._s}{PREFIXES_VIEW} spfx "
-            "WHERE rpfx.token = spfx.token "
+            "SELECT Rpfx.id AS Rid, Spfx.id AS Sid "
+            ", MAX(Rpfx.pos) as RmaxPos, MAX(Spfx.pos) as SmaxPos, count(*) as pfxOverlap "
+            f"FROM {self._R['out_prefix']}{PREFIXES_VIEW} Rpfx, {self._S['out_prefix']}{PREFIXES_VIEW} Spfx "
+            "WHERE Rpfx.token = Spfx.token "
             # length filter
-            f"AND rpfx.len >= (spfx.len * {self._t})"
-            f"AND spfx.len >= (rpfx.len * {self._t})"
+            f"AND Rpfx.len >= (Spfx.len * {self._t})"
+            f"AND Spfx.len >= (Rpfx.len * {self._t})"
             # positional filter
-            "AND LEAST((rpfx.len - rpfx.pos + 1), (spfx.len - spfx.pos + 1)) >= "
-            f"((rpfx.len + spfx.len) * {self._t} / (1 + {self._t})) "
-            "GROUP BY rpfx.id, spfx.id "
+            "AND LEAST((Rpfx.len - Rpfx.pos + 1), (Spfx.len - Spfx.pos + 1)) >= "
+            f"((Rpfx.len + Spfx.len) * {self._t} / (1 + {self._t})) "
+            "GROUP BY Rpfx.id, Spfx.id "
             ")"
         ).execute(
-            f"drop table if exists {PREFIXES_VIEW}"
-        ).execute(
-            f"drop table if exists {self._r}{PREFIXES_VIEW};"
-            f"drop table if exists {self._s}{PREFIXES_VIEW};"
+            f"drop table if exists {self._R['out_prefix']}{PREFIXES_VIEW};"
+            f"drop table if exists {self._S['out_prefix']}{PREFIXES_VIEW};"
         )
 
     def matches(self):
         self._con.execute(
-            f"drop table if exists {self._out_table_name}"
+            f"drop table if exists {self._out_table}"
         ).execute(
-            f"create table {self._out_table_name} as "
-            f"select R.id as {self._l_out_prefix}{self._l_key_attr}, S.id as {self._r_out_prefix}{self._r_key_attr} "
-            f"from {self._r}{TOKENS_DOC_FREQ_VIEW} R, {self._s}{TOKENS_DOC_FREQ_VIEW} S, {CANDIDATE_SET_VIEW} c "
+            f"create table {self._out_table} as "
+            f"select R.id as {self._R['out_prefix']}{self._l['key_attr']}, S.id as {self._S['out_prefix']}{self._r['key_attr']} "
+            f"from {self._R['out_prefix']}{TOKENS_DOC_FREQ_VIEW} R, {self._S['out_prefix']}{TOKENS_DOC_FREQ_VIEW} S, {CANDIDATE_SET_VIEW} c "
             "where c.Rid = R.id "
             "and c.Sid = S.id "
             "and R.token = S.token "
@@ -452,18 +423,18 @@ class _JaccardJoin(_JaccardTemplateJoin):
             "group by R.id, S.id, R.len, S.len, pfxOverlap "
             f"having count(*) + pfxOverlap - 1 >= ((R.len + S.len) * {self._t} / (1+{self._t}))"
         ).execute(
-            f"drop table if exists l_{TOKENS_DOC_FREQ_VIEW};"
-            f"drop table if exists r_{TOKENS_DOC_FREQ_VIEW};"
+            f"drop table if exists {self._l['out_prefix']}{TOKENS_DOC_FREQ_VIEW};"
+            f"drop table if exists {self._r['out_prefix']}{TOKENS_DOC_FREQ_VIEW};"
         ).execute(
             f"drop table if exists {CANDIDATE_SET_VIEW}"
         )
 
     def matches_brute_force(self):
         self._con.execute(
-            f"drop table if exists {self._out_table_name}"
+            f"drop table if exists {self._out_table}"
         ).execute(
-            f"create table {self._out_table_name} as "
-            f"select L.id as {self._l_out_prefix}{self._l_key_attr}, R.id as {self._r_out_prefix}{self._r_key_attr} "
+            f"create table {self._out_table} as "
+            f"select L.id as {self._l['out_prefix']}{self._l['key_attr']}, R.id as {self._r['out_prefix']}{self._r['key_attr']} "
             f"from l_{TOKENS_VIEW} as L, r_{TOKENS_VIEW} as R "
             "where L.token = R.token "
             "group by L.id, L.len, R.id, R.len "
@@ -481,9 +452,10 @@ class _JaccardJoin(_JaccardTemplateJoin):
             f"drop table if exists r_{TOKENS_VIEW};"
             f"drop table if exists full_outer_{DOC_FREQ_VIEW};"
             f"drop table if exists {DOC_FREQ_VIEW};"
-            f"drop table if exists l_{TOKENS_DOC_FREQ_VIEW};"
-            f"drop table if exists r_{TOKENS_DOC_FREQ_VIEW};"
-            f"drop table if exists {PREFIXES_VIEW};"
+            f"drop table if exists {self._l['out_prefix']}{TOKENS_DOC_FREQ_VIEW};"
+            f"drop table if exists {self._r['out_prefix']}{TOKENS_DOC_FREQ_VIEW};"
+            f"drop table if exists {self._l['out_prefix']}{PREFIXES_VIEW};"
+            f"drop table if exists {self._r['out_prefix']}{PREFIXES_VIEW};"
             f"drop table if exists {CANDIDATE_SET_VIEW};"
         )
 
@@ -498,11 +470,27 @@ class _JaccardJoin(_JaccardTemplateJoin):
             r_join_attr: str,
             tokenizer: tokenizers.Tokenizer,
             threshold: float,
-            out_table_name: str,
+            out_table: str,
             l_out_prefix: str,
             r_out_prefix: str
     ):
-        super().__init__(
-            con, l_table, r_table, l_key_attr, r_key_attr, l_join_attr, r_join_attr, tokenizer, threshold,
-            out_table_name, l_out_prefix, r_out_prefix
-        )
+        self._con = con
+        self._t = threshold
+        self._tokenizer = tokenizer
+        self._out_table = out_table
+
+        def to_dict(table, key_attr, join_attr, out_prefix):
+            return {
+                'table': table,
+                'key_attr': key_attr,
+                'join_attr': join_attr,
+                'out_prefix': out_prefix,
+                'count': 0
+            }
+
+        self._l = to_dict(l_table, l_key_attr, l_join_attr, l_out_prefix)
+        self._r = to_dict(r_table, r_key_attr, r_join_attr, r_out_prefix)
+
+        self._widow_placeholder = 0
+        self._R = {}
+        self._S = {}
